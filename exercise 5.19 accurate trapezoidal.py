d24f0ad9ebc8d940ab5000b59b199d3a0aaed526
kevinhakuon <kevinhakuon@gmail.com>
2025-08-07 17:35:18 +0800

    Update exercise 5.19 accurate trapezoidal.py

--- exercise 5.19 accurate trapezoidal.py
+++ exercise 5.19 accurate trapezoidal.py
@@ -5,15 +5,17 @@ from scipy.special import roots_legendre
 from scipy.constants import k, hbar, c
 
 
-tolerance = 1e-8
+tolerance = 1e-4
 func = lambda x : sin(x)**2 / x**2
+x_list = []
 
-def adaptive_trapezoidal (lower_bound, upper_bound, f_lower, f_upper,
+def adaptive_trapezoidal (x_list, lower_bound, upper_bound, f_lower, f_upper,
                         tolerance = tolerance, func = func):   
 
         width = upper_bound - lower_bound
         width_d = width / 2
         mid = (upper_bound + lower_bound) / 2
+        x_list.append(mid)
         f_mid = func(mid)
 
         integral_single = (f_lower + f_upper) * width / 2
@@ -23,13 +25,19 @@ def adaptive_trapezoidal (lower_bound, upper_bound, f_lower, f_upper,
 
         if(error > tolerance):
 
-            integral_fixed = (adaptive_trapezoidal(lower_bound, mid, f_lower, f_mid,
-            tolerance = tolerance / 2)+ adaptive_trapezoidal(mid, upper_bound,
+            integral_fixed = (adaptive_trapezoidal(x_list, lower_bound, mid, f_lower, f_mid,
+            tolerance = tolerance / 2)+ adaptive_trapezoidal(x_list, mid, upper_bound,
             f_mid, f_upper, tolerance = tolerance / 2))
 
             return integral_fixed
         else:
             return integral_double
     
+print(adaptive_trapezoidal(x_list, 0 + tolerance, 10, func(0 + tolerance), func(10)))
+x_list.append(0 + tolerance)
+x_list.append(10)
+x_list.sort()
+y_list = func(np.array(x_list))
+scatter(x_list, y_list, s = 0.5)
+show()
 
-print(adaptive_trapezoidal(0 + tolerance, 10, func(0 + tolerance), func(10)))
