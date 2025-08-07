9504dac756ae46b434ac8a9da1a91c3cd79a1d94
kevinhakuon <kevinhakuon@gmail.com>
2025-08-07 17:37:21 +0800

    Update exercise 5.19 accurate trapezoidal.py

--- exercise 5.19 accurate trapezoidal.py
+++ exercise 5.19 accurate trapezoidal.py
@@ -1,43 +1,52 @@
 import numpy as np
-from numpy import empty, arange, sqrt, sin, cos, pi, exp, linspace
-from pylab import imshow, show, gray, plot, scatter, axhline, loglog
-from scipy.special import roots_legendre
+from numpy import sin, pi
+from pylab import scatter, show
 from scipy.constants import k, hbar, c
 
+# 被積分函數
+def func(x):
+    return sin(x)**2 / x**2
 
-tolerance = 1e-4
-func = lambda x : sin(x)**2 / x**2
-x_list = []
+# 遞迴 adaptive trapezoidal 积分
+def adaptive_trapezoidal(lower_bound, upper_bound, f_lower, f_upper, 
+                         tolerance=1e-4, func=func, sample_points=None):
 
-def adaptive_trapezoidal (x_list, lower_bound, upper_bound, f_lower, f_upper,
-                        tolerance = tolerance, func = func):   
+    if sample_points is None:
+        sample_points = []
 
-        width = upper_bound - lower_bound
-        width_d = width / 2
-        mid = (upper_bound + lower_bound) / 2
-        x_list.append(mid)
-        f_mid = func(mid)
+    width = upper_bound - lower_bound
+    mid = (lower_bound + upper_bound) / 2
+    f_mid = func(mid)
+    sample_points.append(mid)
 
-        integral_single = (f_lower + f_upper) * width / 2
-        integral_double = integral_single / 2 + f_mid * width_d
+    # 單一 trapezoid
+    integral_single = (f_lower + f_upper) * width / 2
+    # 拆兩個 trapezoid
+    integral_double = (f_lower + 2 * f_mid + f_upper) * width / 4
+    error = abs(integral_single - integral_double) / 3
 
-        error = abs((integral_single - integral_double)) / 3
+    if error > tolerance:
+        left = adaptive_trapezoidal(lower_bound, mid, f_lower, f_mid, 
+                                    tolerance / 2, func, sample_points)
+        right = adaptive_trapezoidal(mid, upper_bound, f_mid, f_upper, 
+                                     tolerance / 2, func, sample_points)
+        return left + right
+    else:
+        return integral_double
 
-        if(error > tolerance):
+# Main
+def main():
+    lower = 1e-4
+    upper = 10
+    sample_points = [lower, upper]  # 先放邊界點
 
-            integral_fixed = (adaptive_trapezoidal(x_list, lower_bound, mid, f_lower, f_mid,
-            tolerance = tolerance / 2)+ adaptive_trapezoidal(x_list, mid, upper_bound,
-            f_mid, f_upper, tolerance = tolerance / 2))
+    result = adaptive_trapezoidal(lower, upper, func(lower), func(upper), sample_points=sample_points)
+    print("Integral result =", result)
 
-            return integral_fixed
-        else:
-            return integral_double
-    
-print(adaptive_trapezoidal(x_list, 0 + tolerance, 10, func(0 + tolerance), func(10)))
-x_list.append(0 + tolerance)
-x_list.append(10)
-x_list.sort()
-y_list = func(np.array(x_list))
-scatter(x_list, y_list, s = 0.5)
-show()
+    # 整理與畫圖
+    sample_points = sorted(sample_points)
+    y_list = func(np.array(sample_points))
+    scatter(sample_points, y_list, s=0.5)
+    show()
 
+main()
