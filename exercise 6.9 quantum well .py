ed3b368ea2b252b3872343562b7aa6d6fbd71c3a
kevinhakuon <kevinhakuon@gmail.com>
2025-08-13 02:33:20 +0800

    Create exercise 6.9 quantum well .py

---
+++ exercise 6.9 quantum well .py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from pylab import show
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import sqrt, linspace, sin, cos, pi
+from scipy.constants import epsilon_0, pi, hbar
+
+def H_mn(m, n, width, height, e_mass):
+
+    if(m == n):
+        kinetic = (pi**2 * n**2 * hbar**2) / (2 * e_mass * width)
+        potential = height / 2
+    elif((m%2) != (n%2)):
+        kinetic = 0
+        potential = - (8 * height / pi**2) * (m * n) / (m**2 - n**2)**2
+    else:
+        kinetic = 0
+        potential = 0
+
+    energy = kinetic + potential
+    return energy
+
+def construct_metric(scale,  width, height, e_mass):
+
+    hamilton = np.empty([scale, scale])
+    for row in range (scale):
+        for col in range(scale):
+
+            hamilton[row, col] = H_mn(row + 1, col + 1, width, height, e_mass)
+
+    return hamilton
+
+
+def main():
+
+    m = 1
+    n = 2
+    scale = 10
+    width = 5e-10
+    e_mass = 9.1094e-31
+    e_charge = 16022e-19
+    height = 10
+
+    hamilton = construct_metric(scale, width, height, e_mass)
+    energy = np.linalg.eigvalsh(hamilton)
+
+    print(energy)
+
+main()
