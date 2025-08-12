e5c459415335321b5ba9295613aaacd6cbf77fbd
kevinhakuon <kevinhakuon@gmail.com>
2025-08-13 00:29:43 +0800

    Update exercise 6.8 QR decomposition .py

--- exercise 6.8 QR decomposition .py
+++ exercise 6.8 QR decomposition .py
@@ -18,9 +18,9 @@ def QR_decomp (arr):
 
         u_arr[:, column] = arr[:, column]
 
-        for index in range (column, 0, -1):
+        for index in range (0, column, 1):
             
-            u_arr[:, index] = (arr[:, index] @ q_arr[:, column - index]) * q_arr[:, column - index]
+            u_arr[:, column] -= (arr[:, column] @ q_arr[:, index]) * q_arr[:, index]
 
         u_i = u_arr[:, column]
         q_arr[:, column] = u_i / np.linalg.norm(u_i)
@@ -33,19 +33,47 @@ def QR_decomp (arr):
 
             r_arr[i, j] = q_arr[:, i] @ arr[:, j]
 
-    print(r_arr)
     return(q_arr, r_arr)
+
+def find_eigen(A_i, row, column, tolerance):
+
+    V = np.eye(row)
+    A = A_i.copy()
+    diagonal = False
+
+    while(diagonal == False):
+
+        Q, R = QR_decomp(A)
+        A = R @ Q
+        V = V @ Q
+
+        diagonal = True
+
+        for i in range (row):
+            for j in range(column):
+
+                if(i != j and A[i, j] > tolerance):
+                    diagonal = False
+
+    eigenvalue = A.diagonal()
+
+    return(eigenvalue, V)
+        
 
 
 def main():
 
+    tolerance = 1e-6
     A = np.array([[1, 4, 8, 4],
                   [4, 2, 3, 7],
                   [8, 3, 6, 9],
                   [4, 7, 9, 2]])
     
-    Q, R = QR_decomp(A)
-    print(Q @ R)
+    row, col = A.shape
+    eigenval, eignefunc = find_eigen(A, row, col, tolerance)
+
+    print(eigenval)
+    print(eignefunc)
 
 
 main()
