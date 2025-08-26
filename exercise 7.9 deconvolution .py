d55711f5cb37755f73d8eb8c7d7d3c8947645d1c
kevinhakuon <kevinhakuon@gmail.com>
2025-08-27 00:21:26 +0800

    Create exercise 7.9 deconvolution .py

---
+++ exercise 7.9 deconvolution .py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import math as mt
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import sqrt, linspace, sin, cos, tan, pi, exp
+from scipy import fft
+from scipy.constants import epsilon_0, pi, hbar, h, c, k
+from scipy.special import roots_legendre
+
+def loaddata(name):
+
+    data = np.loadtxt(name)
+    return data
+
+def fft2D(y):
+
+    c = fft.fft2(y)
+
+    return c
+
+def ifft2D(c):
+
+    y = fft.ifft2(c)
+
+    return y
+
+def main():
+
+    sigma = 25
+    data = loaddata("blur.txt")
+    row, col = data.shape
+
+    sample_num = row
+
+    gauss = lambda x, y : exp(-0.5 * (x**2 + y**2) / sigma**2)
+
+    x_list = np.linspace(0, row, sample_num)
+    y_list = np.linspace(0, col, sample_num)
+
+    X, Y = np.meshgrid(x_list, y_list)
+
+    gauss_list = gauss(X, Y)
+
+    g_k = fft2D(gauss_list)
+    b_k = fft2D(data)
+
+    a_k = b_k / (g_k * row * col)
+
+    a = ifft2D(a_k)
+
+    plt.imshow(np.abs(g_k), cmap='gray')
+    plt.show()
+    plt.imshow(np.abs(a), cmap='gray')
+    plt.show()
+
+main()
